9c6ca5293a02b43a93987ed9b8202a5fbb4ec05d
Tolera Imiru <toleeraa2024@gmail.com>
2025-11-30 12:09:17 +0300

    Update connection

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+
 db_url = "postgresql://postgres:tolera@localhost:5432/toli"
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
